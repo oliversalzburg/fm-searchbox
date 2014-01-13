f07f32dae97ab3cab1b02817f041423ca470c9d7
Oliver Salzburg <oliver.salzburg@gmail.com>
2014-01-13 20:37:08 +0100

    Initial commit

---
+++ fm-searchbox.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) 2014, Oliver Salzburg
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Created: 2014-01-13 20:34
+ *
+ * @author Oliver Salzburg
+ * @copyright Copyright (C) 2014, Oliver Salzburg, HARTWIG Communication & Events
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+
+"use strict";
+
+// Declare fmComponents module if it doesn't exist.
+try { angular.module( "fm.components" ); } catch( ignored ) { angular.module( "fm.components", [] ); }
