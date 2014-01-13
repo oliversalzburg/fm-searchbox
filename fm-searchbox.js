d8258cb7996e9ac5e9f4f7750f1981c0890aca2a
Oliver Salzburg <oliver.salzburg@gmail.com>
2014-01-13 20:54:00 +0100

    Initital implementation (pending generalization)

--- fm-searchbox.js
+++ fm-searchbox.js
@@ -30,3 +30,111 @@
 
 // Declare fmComponents module if it doesn't exist.
 try { angular.module( "fm.components" ); } catch( ignored ) { angular.module( "fm.components", [] ); }
+
+angular.module( "fm.components" )
+  .directive( "fmSearchbox", [
+    "$timeout", function( $timeout ) {
+      return {
+        template : "<div>" +
+                   " <button type='button' class='btn btn-default pull-left hideFocus actionButton' data-loading-text={{searchText}} data-toggle='tooltip' title={{actionTitle}} style='margin-right:2px'>" +
+                   "   <i class='icon-search'></i>&nbsp;UID" +
+                   " </button>" +
+                   " <div class='input-group'>" +
+                   "   <input type='text' class='form-control' placeholder={{placeholder}} ng-model=ngModel>" +
+                   "   <span class='input-group-btn'>" +
+                   "     <button type='button' class='btn btn-default hideFocus cancelButton' ng-click=\"ngModel=''\">" +
+                   "       <span class='glyphicon glyphicon-remove'></span>" +
+                   "     </button>" +
+                   "   </span>" +
+                   " </div>" +
+                   "</div>",
+        replace  : true,
+        restrict : "E",
+        scope    : {
+          ngModel           : "=",
+          placeholder       : "@",
+          searchText        : "@",
+          actionTitle       : "@",
+          actionCancelTitle : "@",
+          actionPlaceholder : "@"
+        },
+        link     : function postLink( scope, element, attributes, controller ) {
+
+          scope.actionPending = false;
+
+          var textbox = element.find( "input" );
+          var actionButton = element.find( "button.actionButton" );
+          var cancelButton = element.find( "button.cancelButton" );
+
+          actionButton.hide();
+
+          textbox.on( "focus", function() {
+            actionButton.show();
+          } );
+          textbox.on( "blur", function() {
+            $timeout(
+              function() {
+                // Hide the button if we're not searching right now.
+                // Otherwise, we probably lost focus because the button was clicked.
+                if( scope.actionPending != true ) {
+                  actionButton.fadeOut();
+                }
+              }, 1000
+            );
+          } );
+
+          actionButton.click( function() {
+            waitForUid();
+            var event = scope.$root.$broadcast( "LEARN_UID" );
+            if( event.defaultPrevented ) {
+              stopWaitingForUid();
+            }
+          } );
+
+          cancelButton.click( function() {
+            stopWaitingForUid();
+          } );
+
+          $timeout(
+            function() {
+              actionButton.tooltip( { placement : "top" } );
+              actionButton.removeClass( "actionButton" );
+              cancelButton.removeClass( "cancelButton" );
+            }
+          );
+
+          function waitForUid() {
+            actionButton.button( "loading" );
+            actionButton.tooltip( "hide" );
+            textbox.addClass( "uneditable-input" );
+            cancelButton.tooltip( { placement : "top", title : scope.actionCancelTitle } );
+
+            if( !element.originalPlaceholder ) element.originalPlaceholder = scope.placeholder;
+            scope.actionPending = true;
+            scope.placeholder = scope.actionPlaceholder;
+
+            if( !(scope.$$phase || scope.$root.$$phase) ) scope.$apply();
+          }
+
+          function stopWaitingForUid() {
+            actionButton.button( "reset" );
+            textbox.removeClass( "uneditable-input" );
+            cancelButton.tooltip( "destroy" );
+
+            actionButton.hide();
+            scope.actionPending = false;
+            if( element.originalPlaceholder ) {
+              scope.placeholder = element.originalPlaceholder;
+              element.originalPlaceholder = null;
+            }
+            if( !(scope.$$phase || scope.$root.$$phase) ) scope.$apply();
+          }
+
+          scope.$watch( "actionPending", function( newValue, oldValue ) {
+            if( newValue == true ) waitForUid();
+            if( newValue == false ) stopWaitingForUid();
+          } );
+
+        }
+      };
+    } ] );
